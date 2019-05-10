5c3258cdd20eee071aa1349c1584e57b387f6e9f
htkang <362542580@qq.com>
2019-05-10 09:58:51 +0800

    初始版本

--- src/main.js
+++ src/main.js
@@ -8,12 +8,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  // el: '#app',//  vue默认初始化监听使用的el,可以使用$mount达到一样效果，见【1】
+  el: '#app',
   router,
-  /* components: { App },  // 【2】相同效果
-  template: '<App/>' */
-  render: function (h) { //  【2】
-    return h(App)
-  }
-  // render: h => h(App)   //  【2】
-}).$mount('#app')// 【1】
+  components: { App },
+  template: '<App/>'
+})
